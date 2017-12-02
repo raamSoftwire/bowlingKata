471ab237c0a34e055398597f5a5867814b1bdd5a
raamSoftwire <raam.chauhan@softwire.com>
2017-12-02 20:53:17 +0000

    Passing 14 tests and added miniTest system

--- bowling.js
+++ bowling.js
@@ -3,20 +3,30 @@ const Frame = require('./frame');
 class Game {
 	constructor(rolls) {
 	    this.rolls = rolls;
+	    this.frames = getFrames(rolls);
+  	}
+
+  	displayResults(){
+	console.log(
+		'Rolls: \t'+ this.rolls + '\n '+ 
+		getSimpleTotal(this.frames) + ' ' + 
+		accountForSpares(this.frames)  + ' ' + 
+		accountForStrikes(this.frames));
+	}
+  	
+
+  	displayFrames(){
+  		for(var i in this.frames){
+  			console.log(i);
+  			console.log(this.frames[i]);
+  		}
   	}
 
 	score(){
-
-		var frames = getFrames(this.rolls);
-
-		var simpleTotal = getSimpleTotal(frames);
-
-		var spareBonus = accountForSpares(frames);
-		
-		displayResults(this.rolls, simpleTotal, spareBonus);
-		return simpleTotal + spareBonus;
-
-		// return simpleTotal;
+		var simpleTotal = getSimpleTotal(this.frames);
+		var spareBonus = accountForSpares(this.frames);
+		var strikeBonus = accountForStrikes(this.frames);
+		return simpleTotal + spareBonus+ strikeBonus;
 	}
 }
 
@@ -24,60 +34,91 @@ module.exports = Game;
 
 
 function getFrames(rolls){
-	//only gets 10 frames
-	//ignores fillBall scenario
-	const frames = [];
 
-		var i = 0 ;
+	var frames = [];
 
-		while(i < 9){
+	for(var i = 0; i<rolls.length; i++){
 
-			for (var j=0; j<20; j+=2){
-				if(rolls[j]==10){
-					frames.push(new Frame(10,0));
-					i++;
-				}
-				else{
-					frames.push(new Frame(rolls[j],rolls[j+1]));
-					i++;
-				}
+		if(rolls[i] === 10){
+			if(frames.length === 8){
+				frames.push(new Frame(10,0));
+				frames.push(new Frame(rolls[i+1],0));
+				// console.log('clause 1');
+				break;
+				
+			} 
+			else{
+				frames.push(new Frame(10,0));
+				// console.log('clause 2');
 			}
 		}
+		else{
+			if(frames.length === 9 && rolls[i]+rolls[i+1] === 10){
+				frames.push(new Frame (rolls[i],rolls[i+1]));
+				frames.push(new Frame (rolls[i+2],0));
+				// console.log('clause 3');
+				break;
+				
+			}
+			else{
+				frames.push(new Frame(rolls[i],rolls[i+1]));
+				i++;
+				// console.log('clause 4');
+			}
+		}
+	}
+
+	if(frames.length === 12){
+			frames.pop();
+		}
+
+	if(frames.length === 11){
+		frames[10].roll2 = 0;
+	}
 
 	return frames;
 }
 
+
 function getSimpleTotal(frames){
 	var total = 0;
 
-		for(var i=0; i<frames.length; i++){
+	for(var i=0; i<frames.length; i++){
 
-			total = total + frames[i].total;
-		}
-
-		return total;
+		total += frames[i].total;
+	}
+	return total;
 }
 
 function accountForSpares(frames){
-	//only checks first 9 frames
-	//ignores fillBall scenario
-
 	var spareBonus = 0;
 
 	for(var i=0; i<9; i++){
-		if(frames[i].roll1 + frames[i].roll2 === 10){
-			spareBonus = frames[i+1].roll1;
+		if(frames[i].isSpare){
+			spareBonus += frames[i+1].roll1;
 		}
 	}
-
 	return spareBonus;
 }
 
-function displayResults(rolls,simpleTotal,spareBonus){
-	console.log(
-		'Rolls: \t'+ rolls + 
-		'\nSimple Total: \t' + simpleTotal +
-		'\nSpare Bonus: \t' + spareBonus);
+function accountForStrikes(frames){
+	var strikeBonus = 0;
+
+	for(var i=0; i<9; i++){
+
+		if(frames[i].isStrike){
+			if(frames[i+1].isStrike){
+				strikeBonus += frames[i+1].roll1 + frames[i+2].roll1;
+			}
+			else{
+				strikeBonus +=  frames[i+1].roll1 + frames[i+1].roll2;
+			}
+		}
+	}
+	return strikeBonus;
 }
+
+
+
 
 
--- frame.js
+++ frame.js
@@ -2,23 +2,10 @@ class Frame {
 	constructor(roll1, roll2) {
 	    this.roll1 = roll1;
 	    this.roll2 = roll2;
-	    this.total = roll1 + roll2;   
+	    this.total = roll1 + roll2;
+      this.isStrike = this.roll1 === 10;
+      this.isSpare = this.roll1 + this.roll2 === 10 && this.roll1 !== 10;
   	}
-
-  	isStrike(){
-  		return this.roll1 === 10;
-  	}
-
-  	isSpare(){
-  		return this.roll1 + this.roll2 === 10;
-  	}
-
-    update(points){
-      this.total = this.total + points;
-      return this.total;
-    }
-
-
 }
 
 module.exports = Frame;
